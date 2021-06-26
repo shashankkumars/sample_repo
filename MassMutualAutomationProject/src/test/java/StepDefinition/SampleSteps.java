7e60e76698592b5ee42a7ae6010bd2c3b701ac17
unknown <shashanks132@gmail.com>
2021-06-26 21:06:33 +0530

    created feature file and step definition file

---
+++ MassMutualAutomationProject/src/test/java/StepDefinition/SampleSteps.java
@@ -0,0 +1,28 @@
+package StepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SampleSteps {
+
+	@Given("the user is navigated to the page")
+	public void the_user_is_navigated_to_the_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the values are displayed in the screen")
+	public void the_values_are_displayed_in_the_screen() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the right count of values should be displayed on the screen")
+	public void the_right_count_of_values_should_be_displayed_on_the_screen() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	
+}
---
+++ MassMutualAutomationProject/src/test/resources/Features/sample.feature
@@ -0,0 +1,9 @@
+Feature: Verify count of values
+#Author: Shashank
+
+
+Scenario: To verify the right count of values appear on the screen
+
+Given the user is navigated to the page
+When the values are displayed in the screen
+Then the right count of values should be displayed on the screen
